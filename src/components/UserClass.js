1a9ab5d8ea5663c0393e744e3b618e46aeee790a
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-01-16 06:13:52 +0530

    github api added

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,31 +5,38 @@ class UserClass extends React.Component{
     constructor(props){
         console.log("child constructer")
         super(props);
-        this.state={
-            count:0,
-            count2:2,
+        this.state = {
+            userinfo: {
+                avatar_url:"",
+                login: "dummy",
+                location: "default",
+            },
         };
     }
-    componentDidMount(){
+     async componentDidMount(){
+
+        const data = await fetch("https://api.github.com/users/Srijan1202");
+        const json = await data.json();
+        console.log(json)
+
+        this.setState({
+            userinfo:json
+        });
+        
         console.log("child did mount")
+        console.log(this.state)
     }
     render(){
         console.log("child rendered")
-        const{name,info,contact}=this.props;
-        const{count}=this.state;
+        const{login,location,avatar_url}=this.state.userinfo;
         return(
+           
             <div className="user">
-                <h1>count:{count}</h1>
-                <button onClick={()=>{
-                    this.setState({
-                        count:count+1,
-                    })
-                }}>Increase</button>
-                <h2>{name}</h2>
-                <h3>{info}</h3>
-                <h3>{contact}</h3>
-                {/* <h3>count:{count2}</h3> */}
-            </div>
+                <img src={avatar_url} alt="img" />
+                <h2>Name : {login}</h2>
+                <h3>Location : {location}</h3>
+                {/* <h3>{contact}</h3> */}
+                </div>
         );
     };
 };
