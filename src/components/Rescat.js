f889939d842a89d595f2aa1105c1ecbbee22df06
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-01-18 03:24:03 +0530

    added accordion

---
+++ src/components/Rescat.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Rescategory from "./Rescategory";
+
+const Rescat = (props) =>{
+    const {c} = props;
+    const [show, setshow] = useState(false);
+
+    return(
+        <div className="font- p-5 mx-48 mb-5" key={c.card.card.itemCards[1].card.info.id}>
+          <div
+            className=" flex justify-between border-b mx-6/12 border-grey-200  rounded-lg"
+            onClick={() => {
+                // console.log("clicked");
+                setshow(!show);
+
+            }}
+          >
+            <h1 className="font-extrabold">{c.card.card.title}</h1>
+            <div>⬇️</div>
+          </div>
+          {show && c?.card?.card?.itemCards.map((d) => (
+            
+            <>
+              <Rescategory key={d.card.info.id} detail={d} />
+            </>
+          ))}
+        </div>
+    )
+}
+
+export default Rescat;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,11 +3,14 @@ import Shimmercard from "./Shimmercard";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import Rescategory from "./Rescategory";
+import { useState } from "react";
+import Rescat from "./Rescat";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const { resInfo } = useRestaurantMenu(resId);
-
+ 
   const onlineStatus = useOnlineStatus();
   if (onlineStatus === false) {
     return <h1>You are offline. Please check your connection.</h1>;
@@ -18,11 +21,13 @@ const RestaurantMenu = () => {
   }
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[2]?.card?.card?.info;
-//   const { itemCards } =
-//     resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card?.card;
 
-  const category = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>c?.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-    console.log(category);
+  const category =
+    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
 
   return (
     <div className="">
@@ -33,55 +38,13 @@ const RestaurantMenu = () => {
           {cuisines.join(",")} - {costForTwoMessage}
         </p>
       </div>
-      <h2 className="flex text-center justify-center m-10">Menu</h2>
-       {category.map((c) => (
-        <div className="mx-48 mb-5 flex justify-between border-b mx-6/12 border-grey-200 p-5 m-2 rounded-lg">
-          <h1 className="">{c.card.card.title}</h1>
-          <div>⬇️</div>
-        </div>
-       ))}
-    
-
-
-
-
-
-
-
-
-
-
-
-
       
-      {/* {itemCards.map((item) => (
-        <div key={item.card.info.id} className="itemdis">
-          <ul>
-            <li>
-              <h3>{item.card.info.name}</h3>
-            </li>
-
-            <li>
-              <b>₹{item.card.info.price / 100}</b>
-            </li>
-            <br />
-            <li className="rating">
-              <b>{item.card.info.ratings.aggregatedRating.rating}</b>(
-              {item.card.info.ratings.aggregatedRating.ratingCount})
-            </li>
-            <br />
-            <li>{item.card.info.description}</li>
-          </ul>
-          <img
-            className="itemimg"
-            alt="itemimg"
-            src={
-              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" +
-              item.card.info.imageId
-            }
-          />
-        </div>
-      ))} */}
+      <h2 className="flex text-center justify-center m-10">Menu</h2>
+      {category.map((c) => {
+        
+      return(
+        <Rescat c={c}/>
+      )})}
     </div>
   );
 };
